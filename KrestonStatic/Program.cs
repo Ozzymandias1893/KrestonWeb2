3a1c3da563711a01a3c97bd2e168b1a69c70c054
Vincenzo <vincenzo1893@hotmail.com>
2025-04-22 09:02:24 +0200

    commit

--- KrestonStatic/Program.cs
+++ KrestonStatic/Program.cs
@@ -1,10 +1,18 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+//var builder = WebApplication.CreateBuilder(args);
+//var app = builder.Build();
 
-app.UseDefaultFiles(new DefaultFilesOptions
+//app.UseDefaultFiles(new DefaultFilesOptions
+//{
+//    DefaultFileNames = new List<string> { "index.html" }
+//});
+//app.UseStaticFiles();
+
+//app.Run();
+
+public class Program
 {
-    DefaultFileNames = new List<string> { "index.html" }
-});
-app.UseStaticFiles();
-
-app.Run();
+    public static void Main(string[] args)
+    {
+        // Empty Main to satisfy .NET build, since this is a static website.
+    }
+}
